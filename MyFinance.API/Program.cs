343af9aa595a50ca6f417d002e70f0904bf3b2b3
Gabriel Cézar <gabrielcezar326133@gmail.com>
2026-04-01 12:16:04 -0300

    teste senha

--- MyFinance.API/Program.cs
+++ MyFinance.API/Program.cs
@@ -48,7 +48,7 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
-var defaultConnection = "Host=aws-1-us-east-2.pooler.supabase.com;Port=6543;Database=postgres;Username=postgres.cjyumxatyfvlwgwuwohu;Password=SKvxAbHvUcBeWejU;Pooling=false;CommandTimeout=300;";
+var defaultConnection = "Host=db.cjyumxatyfvlwgwuwohu.supabase.co;Port=5432;Database=postgres;Username=postgres;Password=SKvxAbHvUcBeWejU;CommandTimeout=120;";
 
 void ConfigureDatabase(DbContextOptionsBuilder options) =>
     options.UseNpgsql(
